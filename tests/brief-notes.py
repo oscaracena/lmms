a129f12bc5c570d6793c179817226e907fb3812e
Oscar Aceña <oscaracena@gmail.com>
2022-01-14 16:41:07 +0100

    sync

--- tests/brief-notes.py
+++ tests/brief-notes.py
@@ -3,9 +3,10 @@
 import mido
 import time
 
+note = 37
 port = mido.open_output(name="TEST", virtual=True)
-on = mido.Message("note_on", note=56, velocity=127)
-off = mido.Message("note_off", note=56, velocity=0)
+on = mido.Message("note_on", note=note, velocity=127)
+off = mido.Message("note_off", note=note, velocity=0)
 
 while True:
     print(".", end="", flush=True)
